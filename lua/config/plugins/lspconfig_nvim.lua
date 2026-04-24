d527bca9d98e2b58456369e9183969d71dc0e60b
aiden <xaotag@gmail.com>
2026-04-25 07:57:03 +0800

    change client.supports_method()  to client:supports_method()

--- lua/config/plugins/lspconfig_nvim.lua
+++ lua/config/plugins/lspconfig_nvim.lua
@@ -92,7 +92,6 @@ return {
         if not client then
           return
         end
-
         local map = function(mode, lhs, rhs, desc)
           vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
         end
@@ -126,14 +125,14 @@ return {
         end, "Toggle Diagnostics")
 
         -- Inlay hints toggle
-        if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+        if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
           map("n", "<leader>th", function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
           end, "Toggle Inlay Hints")
         end
 
         -- Document highlight
-        if client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+        if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
           local highlight_group = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
           vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
             buffer = bufnr,
@@ -148,7 +147,7 @@ return {
         end
 
         -- Folding
-        if client.supports_method("textDocument/foldingRange") then
+        if client:supports_method("textDocument/foldingRange") then
           vim.wo.foldexpr = "v:lua.vim.lsp.foldexpr()"
           vim.wo.foldmethod = "expr"
           vim.wo.foldlevel = 99
